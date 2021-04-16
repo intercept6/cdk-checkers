b4458fa6a8bd08d986d6bf1e1d6e746024f8627e
intercept6 <4459220+intercept6@users.noreply.github.com>
2021-04-16 15:59:19 +0900

    🔧 install gts

--- lib/index.ts
+++ lib/index.ts
@@ -2,10 +2,10 @@ import * as cdk from '@aws-cdk/core';
 
 export interface CdkCheckersProps {
   // Define construct properties here
+  dummy?: string;
 }
 
 export class CdkCheckers extends cdk.Construct {
-
   constructor(scope: cdk.Construct, id: string, props: CdkCheckersProps = {}) {
     super(scope, id);
 
